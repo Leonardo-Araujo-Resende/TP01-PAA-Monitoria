6614b92cabba6004bd600d369e4f668147956c55
Leonardo-Araujo-Resende <leonardo.a.aguiar@ufv.br>
2023-10-03 19:03:46 -0300

    teste 1

---
+++ Assets/testeInputField.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+class Pessoa {
+
+    int a = 0;
+    int b = 2;
+    int c = 3;
+
+    void pessoa(int idade,  float altura, float dinheiro){
+        if(a == b){
+            // code
+            for(int i = 0; i < 10; i ++){
+                // code
+            }
+        }
+        else{
+            // code
+        }
+    }
+
+
+
+
+
+
+
+}
